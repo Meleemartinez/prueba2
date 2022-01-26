9cc1c0adba409d622673e96a75b5442d03b34833
Meleemartinez <melisaleemartinez@gmail.com>
2022-01-26 11:54:35 -0300

    probandoPrueba2

---
+++ src/practico_8_ejercicio1_1/Practico_8_Ejercicio1_1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practico_8_ejercicio1_1;
+
+/**
+ *
+ * @author Melisa Lee Martinez Said
+ */
+public class Practico_8_Ejercicio1_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      int i = 0;
+      
+      for (i = 1; i <= 10; i ++){
+      System.out.println ("Hola, ¿qué tal?");
+}
+}
+}
